a55918651a86e2e6a5a933c3cb5508ae921a45f3
eainde <akshay.dipta@gmail.com>
2020-01-04 14:55:50 +0000

    Adding redux persist into the App and persisting cart items to local storage

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import {Provider} from 'react-redux';
-import store from './redux/store';
+import {store, persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 import './index.css';
 
@@ -12,7 +13,9 @@ import './index.css';
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
+        <PersistGate persistor={persistor}>
             <App />
+         </PersistGate>
         </BrowserRouter>
     </Provider>
 , document.getElementById('root'));
